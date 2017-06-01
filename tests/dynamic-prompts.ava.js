5e750a57f420155f2974b4c4cd4d08d637c0dbca
Andrew Worcester <andrew@amwmedia.com>
2017-06-01 07:08:30 -0400

    additional test for dynamic prompts

--- tests/dynamic-prompts.ava.js
+++ tests/dynamic-prompts.ava.js
@@ -14,7 +14,15 @@ test('If prompt is provided as a function, runPrompts() should call it', co.wrap
 	t.true(result.promptFunctionCalled);
 }));
 
-test('If prompt is provided as a function, runPrompts() should cbe called with inquirer instance', co.wrap(function* (t) {
+test('If prompt is provided as a function, runPrompts() should be called with inquirer instance', co.wrap(function* (t) {
 	const result = yield dynamicPrompts.runPrompts();
 	t.is(result.promptArgs[0], plop.inquirer);
+}));
+
+test('Prompt can be a function that syncronously returns answers', co.wrap(function* (t) {
+	const dynPromptSync = plop.setGenerator('dynamic-prompt-sync', {
+		prompts: () => ({ promptFunctionCalled: true })
+	});
+	const result = yield dynPromptSync.runPrompts();
+	t.true(result.promptFunctionCalled);
 }));
