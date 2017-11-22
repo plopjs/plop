ff13c5f82668ba3850b6f20617e125e3a358eedc
Andrew Worcester <andrew@amwmedia.com>
2017-11-22 10:37:17 -0500

    add an additional common action interface check

--- src/actions/_common-action-interface-check.js
+++ src/actions/_common-action-interface-check.js
@@ -1,14 +1,19 @@
-export default function(action) {
+export default function(action, {checkPath=true, checkAbortOnFail=true} = {}) {
 
 	// it's not even an object, you fail!
 	if (typeof action !== 'object') {
 		return `Invalid action object: ${JSON.stringify(action)}`;
 	}
 
-	const {path} = action;
+	const {path, abortOnFail} = action;
 
-	if (typeof path !== 'string' || path.length === 0) {
+	if (checkPath && (typeof path !== 'string' || path.length === 0)) {
 		return `Invalid path "${path}"`;
+	}
+
+	// abortOnFail is optional, but if it's provided it needs to be a Boolean
+	if (checkAbortOnFail && abortOnFail !== undefined && typeof abortOnFail !== 'boolean') {
+		return `Invalid value for abortOnFail (${abortOnFail} is not a Boolean)`;
 	}
 
 	return true;
