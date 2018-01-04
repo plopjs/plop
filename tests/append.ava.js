d9f42e884c7c757c4408f623e8511f837f7dcc80
Andrew Worcester <andrew@amwmedia.com>
2018-01-04 12:28:45 -0500

    changed from async/await to co and yield

--- tests/append.ava.js
+++ tests/append.ava.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import co from 'co';
 import AvaTest from './_base-ava-test';
 const {test, mockPath, testSrcPath, nodePlop} = new AvaTest(__filename);
 
@@ -7,16 +8,16 @@ const plop = nodePlop(`${mockPath}/plopfile.js`);
 const makeList = plop.getGenerator('make-list');
 const appendToList = plop.getGenerator('append-to-list');
 
-test('Check if list has been created', async (t) => {
-	await makeList.runActions({listName: 'test'});
+test('Check if list has been created', co.wrap(function* (t) {
+	yield makeList.runActions({listName: 'test'});
 	const filePath = path.resolve(testSrcPath, 'test.txt');
 	t.true(fs.existsSync(filePath), filePath);
 });
 
-test('Check if entry will be appended', async (t) => {
-	await makeList.runActions({listName: 'list1'});
-	await appendToList.runActions({listName: 'list1', name: 'Marco', allowDuplicates: false});
-	await appendToList.runActions({listName: 'list1', name: 'Polo', allowDuplicates: false});
+test('Check if entry will be appended', co.wrap(function* (t) {
+	yield makeList.runActions({listName: 'list1'});
+	yield appendToList.runActions({listName: 'list1', name: 'Marco', allowDuplicates: false});
+	yield appendToList.runActions({listName: 'list1', name: 'Polo', allowDuplicates: false});
 	const filePath = path.resolve(testSrcPath, 'list1.txt');
 	const content = fs.readFileSync(filePath).toString();
 
@@ -24,12 +25,12 @@ test('Check if entry will be appended', async (t) => {
 	t.is((content.match(/Polo/g) || []).length, 1);
 });
 
-test('Check if duplicates get filtered', async (t) => {
-	await makeList.runActions({listName: 'list2'});
+test('Check if duplicates get filtered', co.wrap(function* (t) {
+	yield makeList.runActions({listName: 'list2'});
 
-	await appendToList.runActions({listName: 'list2', name: 'Marco', allowDuplicates: false});
-	await appendToList.runActions({listName: 'list2', name: 'Polo', allowDuplicates: false});
-	await appendToList.runActions({listName: 'list2', name: 'Marco', allowDuplicates: false});
+	yield appendToList.runActions({listName: 'list2', name: 'Marco', allowDuplicates: false});
+	yield appendToList.runActions({listName: 'list2', name: 'Polo', allowDuplicates: false});
+	yield appendToList.runActions({listName: 'list2', name: 'Marco', allowDuplicates: false});
 	const filePath = path.resolve(testSrcPath, 'list2.txt');
 	const content = fs.readFileSync(filePath).toString();
 
@@ -37,11 +38,11 @@ test('Check if duplicates get filtered', async (t) => {
 	t.is((content.match(/Polo/g) || []).length, 1);
 });
 
-test('Check if duplicates are kept, if allowed', async (t) => {
-	await makeList.runActions({listName: 'list3'});
-	await appendToList.runActions({listName: 'list3', name: 'Marco', allowDuplicates: true});
-	await appendToList.runActions({listName: 'list3', name: 'Polo', allowDuplicates: true});
-	await appendToList.runActions({listName: 'list3', name: 'Marco', allowDuplicates: true});
+test('Check if duplicates are kept, if allowed', co.wrap(function* (t) {
+	yield makeList.runActions({listName: 'list3'});
+	yield appendToList.runActions({listName: 'list3', name: 'Marco', allowDuplicates: true});
+	yield appendToList.runActions({listName: 'list3', name: 'Polo', allowDuplicates: true});
+	yield appendToList.runActions({listName: 'list3', name: 'Marco', allowDuplicates: true});
 	const filePath = path.resolve(testSrcPath, 'list3.txt');
 	const content = fs.readFileSync(filePath).toString();
 
@@ -49,12 +50,12 @@ test('Check if duplicates are kept, if allowed', async (t) => {
 	t.is((content.match(/Polo/g) || []).length, 1);
 });
 
-test('Check if duplicates are only removed below the pattern', async (t) => {
-	await makeList.runActions({listName: 'list4'});
+test('Check if duplicates are only removed below the pattern', co.wrap(function* (t) {
+	yield makeList.runActions({listName: 'list4'});
 
-	await appendToList.runActions({listName: 'list4', name: 'Plop', allowDuplicates: false});
-	await appendToList.runActions({listName: 'list4', name: 'Polo', allowDuplicates: false});
-	await appendToList.runActions({listName: 'list4', name: 'Plop', allowDuplicates: false});
+	yield appendToList.runActions({listName: 'list4', name: 'Plop', allowDuplicates: false});
+	yield appendToList.runActions({listName: 'list4', name: 'Polo', allowDuplicates: false});
+	yield appendToList.runActions({listName: 'list4', name: 'Plop', allowDuplicates: false});
 	const filePath = path.resolve(testSrcPath, 'list4.txt');
 	const content = fs.readFileSync(filePath).toString();
 
