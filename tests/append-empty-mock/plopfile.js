769ac25eba2560ac10cc2c5e0fa6527becb89056
Andrew Worcester <andrew@amwmedia.com>
2018-04-18 17:07:07 -0400

    cleaned up some unnecessary testing files

--- tests/append-empty-mock/plopfile.js
+++ tests/append-empty-mock/plopfile.js
@@ -2,48 +2,17 @@ module.exports = function (plop) {
 	'use strict';
 
 	plop.setGenerator('make-list', {
-		prompts: [
-			{
-				type: 'input',
-				name: 'listName',
-				message: 'What\'s the list name?',
-				validate: function (value) {
-					if ((/.+/).test(value)) { return true; }
-					return 'name is required';
-				}
-			}
-		],
 		actions: [
 			{
 				type: 'add',
 				path: 'src/{{listName}}.txt',
-				templateFile: 'plop-templates/list.txt',
+				template: '',
 			},
 		]
 	});
 
 	plop.setGenerator('append-to-list', {
 		description: 'adds entry to a list',
-		prompts: [
-			{
-				type: 'input',
-				name: 'listName',
-				message: 'What\'s the list name?',
-				validate: function (value) {
-					if ((/.+/).test(value)) { return true; }
-					return 'name is required';
-				}
-			},
-			{
-				type: 'input',
-				name: 'name',
-				message: 'What is your name?',
-				validate: function (value) {
-					if ((/.+/).test(value)) { return true; }
-					return 'name is required';
-				}
-			}
-		],
 		actions: [
 			{
 				type: 'append',
