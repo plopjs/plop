88b89aa548665a80f5bea80dc47aeae3b300249c
William Duclot <william.duclot@gmail.com>
2018-08-19 13:16:23 +0100

    Add file: keep the existing modes

---
+++ tests/add-action-executable-file.ava.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import co from 'co';
+import AvaTest from './_base-ava-test';
+const {test, mockPath, testSrcPath, nodePlop} = (new AvaTest(__filename));
+
+const plop = nodePlop();
+
+/////
+//
+//
+
+test('Add action keeps the executable flag', co.wrap(function* (t) {
+	plop.setGenerator('addExecutable', {
+		actions: [{
+			type: 'add',
+			path: `${testSrcPath}/added.sh`,
+			templateFile: `${mockPath}/plop-templates/add.sh`
+		}]
+	});
+
+	yield plop.getGenerator('addExecutable').runActions();
+	const destStats = fs.statSync(`${testSrcPath}/added.sh`);
+	t.is(destStats.mode & fs.constants.S_IXUSR, fs.constants.S_IXUSR);
+}));
