51b7eddbd26b46ba3f39a147f3c553f54b25f0f9
Andrew Bradley <cspotcode@gmail.com>
2019-07-08 21:19:18 +0000

    move declarations to src to sit alongside js and simplify package.json

---
+++ src/plop.d.ts
@@ -0,0 +1,11 @@
+export {
+    ActionConfig,
+    ActionType,
+    AddActionConfig,
+    AddManyActionConfig,
+    AppendActionConfig,
+    CustomActionFunction,
+    ModifyActionConfig,
+    PlopCfg,
+    PlopGenerator
+} from 'node-plop';
