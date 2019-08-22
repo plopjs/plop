0b78a81de32b6dd15755b9f1cf09fc6ce255da9a
Matthew Doll <matthew.doll@callisonrtkl.com>
2019-08-22 06:30:05 -0500

    improve windows support

--- tests/imported-custom-action-mock/custom-action.js
+++ tests/imported-custom-action-mock/custom-action.js
@@ -1,11 +1,12 @@
 const co = require('co');
 const del = require('del');
 const fspp = require('../../src/fs-promise-proxy');
+import { normalizePath } from '../../src/actions/_common-action-utils';
 
-module.exports = co.wrap(function* (data, cfg/*, plop*/) {
+module.exports = co.wrap(function*(data, cfg /*, plop*/) {
 	const removeFilePath = cfg.path;
 	if (yield fspp.fileExists(removeFilePath)) {
-		return yield del([removeFilePath]);
+		return yield del([normalizePath(removeFilePath)]);
 	} else {
 		throw `Path does not exist ${removeFilePath}`;
 	}
