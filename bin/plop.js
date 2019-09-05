9967032b218466c2a1f85f1644290076b734cd89
ShubhamZanwar <zanwar.shubham@gmail.com>
2019-09-05 14:08:47 +0530

    bin: moving bin code to a separate file

---
+++ bin/plop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+const args = process.argv.slice(2);
+const {Plop, run} = require('../src/plop');
+const argv = require('minimist')(args);
+
+console.log(argv);
+
+Plop.launch({
+	cwd: argv.cwd,
+	configPath: argv.plopfile,
+	require: argv.require,
+	completion: argv.completion
+}, run);
