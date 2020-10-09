19f73f5e5ae88e60f3b8311a8ea15ea6f878a4b9
Robin Knipe <robin.knipe@gmail.com>
2020-10-09 13:43:40 +0100

    Check validation validate function params
    Ensure the `answers` object is also passed to the `validate` function.

--- tests/prompt-bypass-validate.ava.js
+++ tests/prompt-bypass-validate.ava.js
@@ -7,11 +7,23 @@ const plop = nodePlop();
 const prompts = [{
 	type:'input', name:'input', message:'inputMsg',
 	validate: (value) => value === 'invalid' ? 'Is invalid' : true
+}, {
+	type:'input', name:'dependent-input', message:'dependent-inputMsg'
 }];
 
 test('verify valid bypass input', function (t) {
 	const [, isValid] = promptBypass(prompts, ['valid'], plop);
 	t.is(isValid.input, 'valid');
+});
+
+test('verify valid bypass input with access to answers', function (t) {
+	prompts[1].validate = (value, answers) => {
+		t.is(answers.input, 'valid');
+		return !!value;
+	};
+	const [, isValid] = promptBypass(prompts, ['valid', 'also valid'], plop);
+	t.is(isValid.input, 'valid');
+	t.is(isValid['dependent-input'], 'also valid');
 });
 
 test('verify bad bypass input', function (t) {
