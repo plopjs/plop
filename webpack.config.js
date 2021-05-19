aab1ef01b2c1a33501b012cbad5d9d4929f0e718
Evelyn Hathaway <git@evelyn.dev>
2021-05-18 17:57:33 -0700

    fix: export a commonjs module, add code comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,27 @@
 const webpack = require('webpack');
 
+// A webpack plugin that falls back to the default require if the module isn't bundled.
+// - Useful for configuration files from the end user on Node.js
+// - If this breaks, we can either patch it, or decide to not bundle in the next release
 class FallbackRequireWebpackPlugin {
-  apply(compiler) {
-	compiler.hooks.compilation.tap('FallbackRequireWebpackPlugin', (compilation) => {
-		const {renderRequire} = webpack.javascript.JavascriptModulesPlugin.getCompilationHooks(compilation);
-		renderRequire.tap(
-			'FallbackRequireWebpackPlugin',
-			(content, renderContext) => content.replace(
-				/\s*return module\.exports;\s*/,
-				'\nreturn (module.exports && module.exports.name === "webpackEmptyContext") ? require(moduleId) : module.exports;',
-			),
-		);
-	});
-  }
+	apply(compiler) {
+		compiler.hooks.compilation.tap('FallbackRequireWebpackPlugin', (compilation) => {
+			// Permalink for undocumented plugin compilation hook map:
+			// https://github.com/webpack/webpack/blob/v5.37.0/lib/javascript/JavascriptModulesPlugin.js#L123-L165
+			const {renderRequire} = webpack.javascript.JavascriptModulesPlugin.getCompilationHooks(compilation);
+			// Permalink for undocumented plugin compilation hook:
+			// https://github.com/webpack/webpack/blob/v5.37.0/lib/javascript/JavascriptModulesPlugin.js#L1178-L1264
+			renderRequire.tap(
+				'FallbackRequireWebpackPlugin',
+				// Replaces `module.exports` with a commonjs `require` when the exports is a function that will error
+				// on runtime (`webpackEmptyContext`)
+				(content) => content.replace(
+					/\s*return module\.exports;\s*/,
+					'\nreturn (module.exports && module.exports.name === "webpackEmptyContext") ? require(moduleId) : module.exports;',
+				),
+			);
+		});
+	}
 }
 
 module.exports = {
@@ -23,6 +32,16 @@ module.exports = {
 		main: {
 			import: './src/plop.js',
 			filename: './dist/plop.js',
+		},
+		entry: {
+			dependOn: 'main',
+			import: './src/index.js',
+			filename: './dist/index.js',
+			library: {
+				// The entry point of the module should be accessible via commonjs while the rest relies on the webpack
+				// runtime install chunk
+				type: 'commonjs2',
+			},
 		},
 		bin: {
 			dependOn: 'main',
@@ -44,7 +63,9 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			// Replace require extension supports
+			// Replace `require.extension` uses
+			// - Makes sure packages using the deprecated `require.extensions["ts"] = something` will not generate
+			//   Webpack require code
 			{
 				test: /\.js$/,
 				loader: 'string-replace-loader',
