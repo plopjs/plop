4a14c4ddd5af8d22a2a116ab33c012a008af73b4
Corbin Crutchley <crutchcorn@gmail.com>
2021-11-22 20:35:42 -0800

    Improve add action tests

--- tests/actions.spec.js
+++ tests/actions.spec.js
@@ -3,8 +3,8 @@ const { resolve } = require("path");
 const { waitFor } = require("cli-testing-library");
 const fs = require("fs");
 
-test("Plop to add files", async () => {
-  const { findByText, fireEvent } = await renderPlop([""], {
+test("Plop to add and rename files", async () => {
+  const { findByText, fireEvent } = await renderPlop(["addAndNameFile"], {
     cwd: resolve(__dirname, "./examples/add-action"),
     debug: true,
   });
@@ -24,6 +24,33 @@ test("Plop to add files", async () => {
   const data = fs.readFileSync(expectedFilePath, "utf8");
 
   expect(data).toMatch(/Hello/);
+
+  fs.unlinkSync(expectedFilePath);
+
+  fireEvent.sigterm();
+});
+
+test("Plop to add and change file contents", async () => {
+  const { findByText, fireEvent } = await renderPlop(["addAndChangeFile"], {
+    cwd: resolve(__dirname, "./examples/add-action"),
+    debug: true,
+  });
+
+  expect(await findByText("What's your name?")).toBeTruthy();
+
+  fireEvent.type("Corbin");
+  fireEvent.enter();
+
+  const expectedFilePath = resolve(
+    __dirname,
+    "./examples/add-action/output/to-add-change.txt"
+  );
+
+  await waitFor(() => fs.promises.stat(expectedFilePath));
+
+  const data = fs.readFileSync(expectedFilePath, "utf8");
+
+  expect(data).toMatch(/Hi Corbin!/);
 
   fs.unlinkSync(expectedFilePath);
 
--- tests/examples/add-action/plopfile.js
+++ tests/examples/add-action/plopfile.js
@@ -1,5 +1,5 @@
 module.exports = function (plop) {
-  plop.setGenerator("addAndNameFIle", {
+  plop.setGenerator("addAndNameFile", {
     description: "Name that file",
     prompts: [
       {
@@ -13,6 +13,24 @@ module.exports = function (plop) {
         type: "add",
         path: "./output/{{fileName}}.txt",
         templateFile: "./templates/to-add.txt",
+      },
+    ],
+  });
+
+  plop.setGenerator("addAndChangeFile", {
+    description: "Name that file",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What's your name?",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "./output/to-add-change.txt",
+        templateFile: "./templates/to-add-change.txt",
       },
     ],
   });
