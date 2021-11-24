c50946e19045526ddd1d796c142d152b06ca3309
Corbin Crutchley <crutchcorn@gmail.com>
2021-11-23 16:56:46 -0800

    Fix final tests

--- tests/examples/wrap-plop/index.js
+++ tests/examples/wrap-plop/index.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 import path from "node:path";
 import minimist from "minimist";
-// Test has same results with or without instrumentation
-import { Plop, run } from "../../../src/plop.js";
+import { Plop, run } from "../../../instrumented/src/plop.js";
 
 const args = process.argv.slice(2);
 const argv = minimist(args);
@@ -14,7 +13,7 @@ Plop.launch(
   {
     cwd: argv.cwd,
     // In order for `plop` to always pick up the `plopfile.js` despite the CWD, you must use `__dirname`
-    configPath: path.join(__dirname, "plopfile.js"),
+    configPath: path.join(__dirname, "plopfile.cjs"),
     require: argv.require,
     completion: argv.completion,
     // This will merge the `plop` argv and the generator argv.
