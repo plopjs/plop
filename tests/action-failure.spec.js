336c45460b500c8f7a979b16e64649bf0e3237f6
Axel Hecht <axel@pike.org>
2021-12-03 12:41:16 +0100

    Return error code on failures on actions
    Fixes #299

---
+++ tests/action-failure.spec.js
@@ -0,0 +1,22 @@
+import { resolve, dirname } from "node:path";
+import { waitFor } from "cli-testing-library";
+import { renderPlop } from "./render.js";
+
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+test("should load ESM file", async () => {
+  const result = await renderPlop([], {
+    cwd: resolve(__dirname, "./examples/action-failure"),
+  });
+  const { findByText, userEvent } = result;
+  expect(await findByText("What is your name?")).toBeTruthy();
+  userEvent.keyboard("Joe");
+  expect(await findByText("Joe")).toBeTruthy();
+  userEvent.keyboard("[Enter]");
+  const actionOutput = await findByText("Action failed");
+  await waitFor(() =>
+    expect(actionOutput.hasExit()).toStrictEqual({ exitCode: 1 })
+  );
+});
