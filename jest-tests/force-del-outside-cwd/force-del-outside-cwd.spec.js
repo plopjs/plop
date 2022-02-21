d1ceb74d451574fab02bd51288e8d94e902b05f3
Corbin Crutchley <crutchcorn@gmail.com>
2022-02-21 03:16:20 -0800

    chore: jest is hungry. jest requires even more tests to be ported

--- jest-tests/force-del-outside-cwd/force-del-outside-cwd.spec.js
+++ jest-tests/force-del-outside-cwd/force-del-outside-cwd.spec.js
@@ -1,25 +1,25 @@
 import fs from 'fs';
-import AvaTest from './_base-ava-test.js';
-import {fileURLToPath} from 'node:url';
+import nodePlop from '../../src/index.js';
+import {setupMockPath} from "../helpers/path.js";
+const {clean, testSrcPath, mockPath} = setupMockPath(import.meta.url);
 
-const __filename = fileURLToPath(import.meta.url);
-const {test, mockPath, testSrcPath, nodePlop} = (new AvaTest(__filename));
+afterEach(clean);
 
 var plop;
-test.before(async () => {
+beforeEach(async () => {
 	plop = await nodePlop(`${mockPath}/sub/plopfile.js`);
 });
 
 // chdir doesn't like to work in modern versions of ava (or many other test frameworks)
 // EG: process.chdir() is not supported in workers
 // We should rewrite this test
-test.skip('Force del outside cwd test', async function (t) {
+test('Force del outside cwd test', async function () {
 	process.chdir(`${mockPath}/sub`);
 	fs.mkdirSync(testSrcPath);
 	fs.writeFileSync(testSrcPath + '/test.txt', 'init content');
 	const testGen = plop.getGenerator('test');
 	const {changes} = await testGen.runActions();
 	const content = fs.readFileSync(testSrcPath + '/test.txt', 'utf8');
-	t.is(changes.length, 1);
-	t.is(content, 'test content');
+	expect(changes.length).toBe(1);
+	expect(content).toBe('test content');
 });
