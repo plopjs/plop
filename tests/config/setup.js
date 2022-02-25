13fe1419ad21debd430bc946a952b110ff343e75
Corbin Crutchley <crutchcorn@gmail.com>
2022-02-25 00:25:59 -0800

    chore: fix tests for Windows, enable CI

--- tests/config/setup.js
+++ tests/config/setup.js
@@ -4,7 +4,7 @@ import { configure } from "cli-testing-library";
 import "cli-testing-library/extend-expect";
 
 configure({
-  asyncUtilTimeout: 2000,
-  renderAwaitTime: 1000,
-  errorDebounceTimeout: 1000,
+  asyncUtilTimeout: 8000,
+  renderAwaitTime: 4000,
+  errorDebounceTimeout: 4000,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,7 @@ export default defineConfig({
     test: {
         globals: true,
         setupFiles: ['./tests/config/setup.js'],
+        testTimeout: 10000,
+        hookTimeout: 10000
     },
 })
