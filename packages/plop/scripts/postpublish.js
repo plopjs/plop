46c44b27e896f314a6398a2e2eea02a3dbb4a1bf
Corbin Crutchley <git@crutchcorn.dev>
2022-04-14 01:02:04 -0700

    chore: add in root README with proper documentation

---
+++ packages/plop/scripts/postpublish.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import fs from "fs";
+import * as path from "path";
+import * as url from "url";
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+const exists = fs.existsSync("./README.md");
+
+if (exists) {
+  // Restore version of README previously there.
+  fs.copyFileSync(
+    path.join(__dirname, "./README.md"),
+    path.join(__dirname, "../README.md")
+  );
+
+  fs.unlinkSync(path.join(__dirname, "./README.md"));
+}
